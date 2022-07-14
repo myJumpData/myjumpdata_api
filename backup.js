42b5321ca13517be39e7c7d10280298da0c44035
Marko Fediv <marko.fediv@gmail.com>
2022-07-14 21:54:42 +0200

    fix

--- backup.js
+++ backup.js
@@ -1,5 +1,20 @@
 const { MongoTools } = require("node-mongotools");
-const { CONNECT_STRING_DEFAULT, ENV_DATA } = require("./src/consts/db");
+const ENV_DATA = {
+  DB_USER: process.env["DB_USER"],
+  DB_PSWD: process.env["DB_PSWD"],
+  DB_HOST: process.env["DB_HOST"],
+  DB_PORT: process.env["DB_PORT"],
+  DB_NAME: process.env["DB_NAME"],
+};
+
+if (ENV_DATA.DB_HOST === null || ENV_DATA.DB_HOST === "") {
+  ENV_DATA.DB_HOST = "localhost";
+}
+if (ENV_DATA.DB_PORT === null || ENV_DATA.DB_PORT === "") {
+  ENV_DATA.DB_PORT = "27017";
+}
+
+const CONNECT_STRING_DEFAULT = `mongodb://${ENV_DATA.DB_HOST}:${ENV_DATA.DB_PORT}`;
 
 const mongoTools = new MongoTools();
 const mtOptions = {
